ae36236a5d2f0a8684d8588301768d00a3302c5a
PiyushPawar18 <143932175+PiyushPawar18@users.noreply.github.com>
2024-07-12 22:31:08 +0530

    CityGuide.cs

---
+++ Basic Programs/CityGuide.cs
@@ -0,0 +1,34 @@
+using System;
+
+class CityGuide
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Select your choice");
+        Console.WriteLine("London");
+        Console.WriteLine("Bombay");
+        Console.WriteLine("Paris");
+        Console.WriteLine("Type your choice");
+
+        String name = Console.ReadLine();
+
+        switch(name)
+        {
+            case "Bombay":
+                Console.WriteLine("Bombay: Guide 5");
+                break;
+
+            case "London":
+                Console.WriteLine("London: Guide 10");
+                break;
+
+            case "Paris":
+                Console.WriteLine("Paris: Guide 15");
+                break;
+
+            default:
+                Console.WriteLine("Invalid Choice");
+		break;
+		}
+	}
+}
